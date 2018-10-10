d0f2a852013c7943513c4da1e961db3ff5126307
rajesh <rajesh.chauhan23102008@gmail.com>
2018-10-11 00:24:55 +0530

    recipe model and ui design for recipe list and detail

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
     selector: 'app-recipe-list',
     templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent {
+    recipes: Recipe[] = [
+        new Recipe('Test Recipe', 'This is my First Test Recipe',
+            'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d'),
+        new Recipe('Test Recipe', 'This is my First Test Recipe',
+                'https://c.pxhere.com/photos/8b/0f/food_meat_recipe_power_pork_dishes-604134.jpg!d')
+        ];
 
 }
