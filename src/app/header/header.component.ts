98c86df95c99ad7838b54602d0b08aff7aa40df7
rajesh <rajesh.chauhan23102008@gmail.com>
2018-11-12 20:33:09 +0530

    condition added for header menu page display

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 
 @Component({
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+
+    @Output() headerClick = new EventEmitter<string>();
+
+    onHeaderClick(menuSelected: string) { this.headerClick.emit(menuSelected); }
+
 
 }
