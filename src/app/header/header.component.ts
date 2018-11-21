425d485d6be7a7c9c1f9413c6801f0f5f271f781
rajesh <rajesh.chauhan23102008@gmail.com>
2018-11-21 12:08:39 +0530

    Revert "condition added for header menu page display"
    This reverts commit 98c86df95c99ad7838b54602d0b08aff7aa40df7.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 
 
 @Component({
@@ -6,10 +6,5 @@ import { Component, Output, EventEmitter } from '@angular/core';
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-
-    @Output() headerClick = new EventEmitter<string>();
-
-    onHeaderClick(menuSelected: string) { this.headerClick.emit(menuSelected); }
-
 
 }
